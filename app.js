6f59038d6cf3266512704edfacdbe713a50c9c1a
sugamitu2005-lab <sugamitu2005@gmail.com>
2026-06-06 00:02:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // POSTデータを受け取るための設定
 app.use(express.urlencoded({ extended: true }));
